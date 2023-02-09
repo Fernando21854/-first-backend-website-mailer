96d11bfa6a7249c3a7ab57acdb72516c4f3a23f7
Fernando Vargas <60250423+Fernando21854@users.noreply.github.com>
2023-02-08 17:01:34 -0800

    Update signuppage2.php

---
+++ signuppage2.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <title>Lab4</title>
+
+    <head>
+        SIGNED IN<br><br>
+    </head>
+    <body>
+        
+    <p>You are signed in! Press LOG IN to continue. <a href="login.php">LOG IN</a>  </p>
+        
+
+        
+    </body>
+</html>
